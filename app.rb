f0d78a95538afe83e6c6ed5152ccd8f8cf165952
simonwinbladnti <152278872+simonwinbladnti@users.noreply.github.com>
2024-11-11 12:54:42 +0100

    Update week 35

--- app.rb
+++ app.rb
@@ -1,7 +1,18 @@
 class App < Sinatra::Base
 
-    get '/' do
-        erb(:"index")
+    def db
+        return @db if @db
+
+        @db = SQLite3::Database.new("db/todo.sqlite")
+        @db.results_as_hash = true
+
+        return @db
     end
+
+    get '/' do
+        @todo = db.execute('SELECT * FROM todo')
+        p @todo
+        erb(:"index")
+    end 
 
 end
--- db/seeder.rb
+++ db/seeder.rb
@@ -1,11 +1,48 @@
 require 'sqlite3'
 
 class Seeder
-
+  
   def self.seed!
-    p "doit"
+    drop_tables
+    create_tables
+    populate_tables
   end
 
+  def self.drop_tables
+    db.execute('DROP TABLE IF EXISTS users')
+    db.execute('DROP TABLE IF EXISTS todo')
+  end
+
+  def self.create_tables
+    db.execute('CREATE TABLE users (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      username TEXT NOT NULL,
+      password VARCHAR(255) NOT NULL
+    )')
+    db.execute('CREATE TABLE todo (
+      todo_id INTEGER PRIMARY KEY AUTOINCREMENT,
+      id INTEGER NOT NULL,
+      label TEXT NOT NULL,
+      description TEXT NOT NULL,
+      date_created INTEGER NOT NULL,
+      date_expire INTEGER,
+      status INTEGER NOT NULL
+    )')
+  end
+
+  def self.populate_tables
+    db.execute('INSERT INTO todo (todo_id, id, label, description, date_created, date_expire, status) VALUES (1, 1, "Köpa Ägg", "12 pack stora ägg, rabatt på ICA", 20241104, 20241105, 0 )')
+
+  end
+
+  private
+  def self.db
+    return @db if @db
+    @db = SQLite3::Database.new('db/todo.sqlite')
+    @db.results_as_hash = true
+    @db
+  end
 end
+
 
 Seeder.seed!
